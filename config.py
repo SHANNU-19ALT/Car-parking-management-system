fa4f5bdee412aaf73f35a9c1e6ae09fe590fe408
SHANNU-19ALT <shanmukhbadam@gmail.com>
2025-03-11 00:49:34 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///parking.db'  # Use MySQL in production
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.urandom(24)
